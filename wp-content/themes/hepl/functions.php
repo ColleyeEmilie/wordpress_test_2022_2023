d33882cc8ea0ff7a2bba1a4a2400ca67b0c7c00c
emilie <colleyeemilie@hotmail.com>
2023-03-31 17:04:36 +0200

    add contact form

--- wp-content/themes/hepl/functions.php
+++ wp-content/themes/hepl/functions.php
@@ -1,71 +1,159 @@
 <?php
 
-// Désactiver l'éditeur "Gutenberg" de Wordpress
+// Démarrer le système de sessions pour pouvoir afficher des messages de feedback venant des formulaires.
+if(session_status() === PHP_SESSION_NONE) session_start();
+
+// Charger les fichiers des fonctionnalités extraites dans des classes.
+require_once(__DIR__ . '/controllers/ContactForm.php');
+
+// Disable Wordpress' default Gutenberg Editor:
 add_filter('use_block_editor_for_post', '__return_false', 10);
 
-//Navigation menu
-register_nav_menu('main', 'Navigation principale du site web (en-tête) ');
+// Register existing navigation menus
+register_nav_menu('main', 'Navigation principale du site web (en-tête)');
 register_nav_menu('footer', 'Navigation de pied de page');
-register_nav_menu('social-media', 'liens vers les réseaux sociaux');
+register_nav_menu('social-media', 'Liens vers les réseaux sociaux');
 
-//custom function that returns a menu structure for giver location
+// Custom function that returns a menu structure for given location
 function hepl_get_menu(string $location, ?array $attributes = []): array
 {
-    // 1. récupérer les liens en base de données pour la location $location
+    // 1. Récupérer les liens en base de données pour la location $location
     $locations = get_nav_menu_locations();
     $menuId = $locations[$location];
     $items = wp_get_nav_menu_items($menuId);
-    //2. formater les liens récupérés en objet qui contiennent les attributs suivants
-        // - href : l'URL complete pour ce lien
-        // - label : le libellé affichable pour ce lien
-    $links = [];
-    foreach($items as $item){
-        $link = new stdClass(); // crée un objet vide qui ne contient rien du tout.
-        $link->href = $item->url;
-        $link->label= $item->title;
 
-        foreach($attributes as $attribute){
-            $link->$attribute = get_field($attribute,$item->ID);
+    // 2. Formater les liens récupérés en objets qui contiennent les attributs suivants :
+    // - href : l'URL complète pour ce lien
+    // - label : le libellé affichable pour ce lien
+    $links = [];
+
+    foreach ($items as $item) {
+        $link = new stdClass();
+        $link->href = $item->url;
+        $link->label = $item->title;
+
+        foreach($attributes as $attribute) {
+            $link->$attribute = get_field($attribute, $item->ID);
         }
+
         $links[] = $link;
     }
-    //3. Retourner l'ensemble des liens formatés en un seul tableau non-associatif
+
+    // 3. Retourner l'ensemble des liens formatés en un seul tableau non-associatif
     return $links;
-};
+}
 
-//activer les images thumbnail sur nos posts
+// Activer les images "thumbnail" sur nos posts
 add_theme_support('post-thumbnails');
-add_image_size('animal_thumbnail', 400, 400,true);
+add_image_size('animal_thumbnail', 400, 400, true);
 
-//enregistrer un custom post type
-function hepl_register_custom_post_types(): void
+// Enregistrer un custom post type :
+function hepl_register_custom_post_types()
 {
-    register_post_type('animal',
-    [
-        'label'=>'Animaux',
-        'description'=>' blablabla ',
-        'public'=> true,
-        'menu_position'=>20,
-        'menu_icon'=>'dashicons-pets',
-        'supports'=>['title', 'thumbnail'], //ce qu'on garde dans l'éditeur.
+    register_post_type('animal', [
+        'label' => 'Animaux',
+        'description' => 'Les animaux disponibles dans notre refuge.',
+        'public' => true,
+        'menu_position' => 20,
+        'menu_icon' => 'dashicons-pets', // https://developer.wordpress.org/resource/dashicons/#pets,
+        'supports' => ['title','thumbnail'],
     ]);
-};
+
+    register_post_type('message', [
+        'label' => 'Message de contact',
+        'description' => 'Les messages envoyés via le formulaire de contact.',
+        'public' => true,
+        'menu_position' => 20,
+        'menu_icon' => 'dashicons-email', // https://developer.wordpress.org/resource/dashicons/#pets,
+        'supports' => ['title','editor'],
+    ]);
+}
+
 add_action('init', 'hepl_register_custom_post_types');
 
-//charger les traductions existantes
-load_theme_textdomain('hepl', get_template_directory() . '/translation');
+// Charger les traductions existantes
+load_theme_textdomain('hepl', get_template_directory() . '/locales');
 
-//ajouter le système personnalisé de remplacement des variables dans les phrases traduisibles
-function __hepl(string $translation, array $replacements=[]){
-    //1. récupérer la traduction de la phrase présente dans $translation
+// Ajouter le système personnalisé de remplacement des variables dans les phrases traduisibles
+// ex: $replacements = ['name' => 'Jean-Paul']
+function __hepl(string $translation, array $replacements = [])
+{
+    // 1. Récupérer la traduction de la phrase présente dans $translation
     $base = __($translation, 'hepl');
 
-    //2. On va remplacer toutes les occurences des variables par leurs valeurs.
-    foreach ($replacements as $key =>$value){
+    // 2. Remplacer toutes les occurrences des variables par leur valeur
+    foreach ($replacements as $key => $value) {
         $variable = ':' . $key;
         $base = str_replace($variable, $value, $base);
     }
 
-    //3. On va retourner la traduction complète.
+    // 3. Retourner la traduction complète.
     return $base;
+}
+
+// Gérer le formulaire de contact "custom"
+// Inspiré de : https://wordpress.stackexchange.com/questions/319043/how-to-handle-a-custom-form-in-wordpress-to-submit-to-another-page
+
+function hepl_execute_contact_form()
+{
+    $config = [
+        'nonce_field' => 'contact_nonce',
+        'nonce_identifier' => 'hepl_contact_form',
+    ];
+
+    (new \Hepl\ContactForm($config, $_POST))
+
+   ->validate([
+        'firstname' => ['required'],
+        'lastname' => ['required'],
+        'email' => ['required','email'],
+        'message' => [],
+    ])
+
+    ->sanitize([
+        'firstname' => 'text_field',
+        'lastname' => 'text_field',
+        'email' => 'email',
+        'message' => 'textarea_field',
+    ])
+
+    ->save(
+        title: fn($data) => $data['firstname'] . ' ' . $data['lastname'] . ' <' . $data['email'] . '>',
+        content: fn($data) => $data['message'],
+    )
+
+    ->send(
+        title: fn($data) => 'Nouveau message de ' . $data['firstname'] . ' ' . $data['lastname'],
+        content: fn($data) => 'Prénom: ' . $data['firstname'] . PHP_EOL . 'Nom: ' . $data['lastname'] . PHP_EOL . 'Email: ' . $data['email'] . PHP_EOL . 'Message:' . PHP_EOL . $data['message'],
+    )
+
+    ->feedback();
+}
+
+add_action('admin_post_nopriv_hepl_contact_form', 'hepl_execute_contact_form');
+add_action('admin_post_hepl_contact_form', 'hepl_execute_contact_form');
+
+// Travailler avec la session de PHP
+function hepl_session_flash(string $key, mixed $value)
+{
+    if(! isset($_SESSION['hepl_flash'])) {
+        $_SESSION['hepl_flash'] = [];
+    }
+
+    $_SESSION['hepl_flash'][$key] = $value;
+}
+
+function hepl_session_get(string $key)
+{
+    if(isset($_SESSION['hepl_flash']) && array_key_exists($key, $_SESSION['hepl_flash'])) {
+        // 1. Récupérer la donnée qui a été flash.
+        $value = $_SESSION['hepl_flash'][$key];
+        // 2. Supprimer la donnée de la session.
+        unset($_SESSION['hepl_flash'][$key]);
+        // 3. Retourner la donnée récupérée.
+        return $value;
+    }
+
+    // La donnée n'existait pas dans la session flash, on retourne null.
+    return null;
 }
--- wp-content/themes/hepl/template-about.php
+++ wp-content/themes/hepl/template-about.php
@@ -1,25 +1,81 @@
 <?php /* Template Name: About page template */ ?>
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<main class="page">
-    <div class="page__main">
-        <h2 class="page__title"><?= get_the_title();?></h2>
-        <div class="page__content">
-            <?php the_content(); ?>
+<?php if(have_posts()): while(have_posts()): the_post(); ?>
+    <main class="page">
+        <div class="page__main">
+            <h2 class="page__title"><?= get_the_title(); ?></h2>
+            <div class="page__content">
+                <?php the_content(); ?>
+            </div>
         </div>
-    </div>
-    <div class="page__form">
-        <?= apply_filters('the_content', '[contact-form-7 id="60" title="Contactez-moi"]')?>
-    </div>
-    <aside class="page__aside profile">
-        <h2 class="profile__name"><?= get_field('name')?></h2>
-        <div class="profile__description"><?= get_field('description')?></div>
-        <figure class="profile__picture">
-            <?php $avatar = get_field('avatar'); ?>
-            <img src="<?= $avatar['sizes']['medium_large']?>" alt="<?= $avatar['alt'];?>">
-        </figure>
-    </aside>
-</main>
-<?php endwhile; endif; ?>
+        <div class="page__form" style="margin: 5em 0; padding: 5em 0; border-top: 1px solid black; border-bottom: 1px solid black;">
+            <?php
+            $feedback = hepl_session_get('hepl_contact_form_feedback') ?? false;
+            $errors = hepl_session_get('hepl_contact_form_errors') ?? [];
+            ?>
 
+            <?php if($feedback): ?>
+                <div class="success" style="border:green;padding:1em;color: green;">
+                    <p>Merci&nbsp;! Votre message a bien été envoyé.</p>
+                </div>
+            <?php else: ?>
+
+                <?php if($errors): ?>
+                    <div class="error" style="border:red;padding:1em;color: red;">
+                        <p>Attention&nbsp;! Merci de corriger les erreurs du formulaire.</p>
+                    </div>
+                <?php endif; ?>
+
+                <form action="<?= esc_url(admin_url('admin-post.php')); ?>" method="POST" class="contact">
+                    <fieldset class="contact__info">
+                        <div class="field">
+                            <label for="firstname" class="field__label">Votre prénom</label>
+                            <input type="text" name="firstname" id="firstname" class="field__input" />
+                            <?php if($errors['firstname'] ?? null): ?>
+                                <p class="field__error" style="color:red"><?= $errors['firstname']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                        <div class="field">
+                            <label for="lastname" class="field__label">Votre nom</label>
+                            <input type="text" name="lastname" id="lastname" class="field__input" />
+                            <?php if($errors['lastname'] ?? null): ?>
+                                <p class="field__error" style="color:red"><?= $errors['lastname']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                        <div class="field">
+                            <label for="email" class="field__label">Votre adresse mail</label>
+                            <input type="email" name="email" id="email" class="field__input" />
+                            <?php if($errors['email'] ?? null): ?>
+                                <p class="field__error" style="color:red"><?= $errors['email']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                        <div class="field">
+                            <label for="message" class="field__label">Votre message</label>
+                            <textarea name="message" id="message" cols="30" rows="10" class="field__textarea"></textarea>
+                            <?php if($errors['message'] ?? null): ?>
+                                <p class="field__error" style="color:red"><?= $errors['message']; ?></p>
+                            <?php endif; ?>
+                        </div>
+                    </fieldset>
+                    <div class="contact__footer">
+                        <input type="hidden" name="action" value="hepl_contact_form" />
+                        <input type="hidden" name="contact_nonce" value="<?= wp_create_nonce('hepl_contact_form'); ?>" />
+                        <button class="contact__submit" type="submit">Envoyer</button>
+                    </div>
+                </form>
+            <?php endif; ?>
+        </div>
+        <div class="page__form">
+            <?= apply_filters('the_content', '[contact-form-7 id="56" title="Formulaire page de contact"]'); ?>
+        </div>
+        <aside class="page__aside profile">
+            <h2 class="profile__name"><?= get_field('name'); ?></h2>
+            <div class="profile__description"><?= get_field('description'); ?></div>
+            <figure class="profile__picture">
+                <?php $avatar = get_field('avatar'); ?>
+                <img src="<?= $avatar['sizes']['medium_large']; ?>" alt="<?= $avatar['alt']; ?>" />
+            </figure>
+        </aside>
+    </main>
+<?php endwhile; endif; ?>
 <?php get_footer(); ?>
